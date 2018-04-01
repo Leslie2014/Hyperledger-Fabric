076a363d1012b4d7c74b4a238d62d75643ba243c
jiajun.hu <jiajun.hu@dianrong.com>
2018-04-01 11:24:58 +0800

    temp: build

--- conf.py
+++ conf.py
@@ -111,27 +111,23 @@ htmlhelp_basename = 'HyperledgerFabricdoc'
 
 # -- Options for LaTeX output ------------------------------------------------
 
-latex_elements = {
-    'preamble': "".join((
-        '\DeclareUnicodeCharacter{00A0}{ }',  # NO-BREAK SPACE
-        '\DeclareUnicodeCharacter{251C}{+}',  # BOX DRAWINGS LIGHT VERTICAL AND RIGHT
-        '\DeclareUnicodeCharacter{2514}{+}',  # BOX DRAWINGS LIGHT UP AND RIGHT
-    ))
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
+latex_elements={# The paper size ('letterpaper' or 'a4paper').
+'papersize':'a4paper',# The font size ('10pt', '11pt' or '12pt').
+'pointsize':'12pt','classoptions':',oneside','babel':'',#必須
+'inputenc':'',#必須
+'utf8extra':'',#必須
+# Additional stuff for the LaTeX preamble.
+'preamble': r"""
+\usepackage{xeCJK}
+\usepackage{indentfirst}
+\setlength{\parindent}{2em}
+\setCJKmainfont{WenQuanYi Micro Hei}
+\setCJKmonofont[Scale=0.9]{WenQuanYi Micro Hei Mono}
+\setCJKfamilyfont{song}{WenQuanYi Micro Hei}
+\setCJKfamilyfont{sf}{WenQuanYi Micro Hei}
+\XeTeXlinebreaklocale "zh"
+\XeTeXlinebreakskip = 0pt plus 1pt
+"""
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
